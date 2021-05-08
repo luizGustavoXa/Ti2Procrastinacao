9f52df589bef1b7364f81dddf1ac42dc59e54040
luizGustavoXa <luiz.alcantara8@gmail.com>
2021-05-07 23:03:00 -0300

    teste

--- ti2/src/main/java/app/Aplicacao.java
+++ ti2/src/main/java/app/Aplicacao.java
@@ -2,15 +2,22 @@ package app;
 
 import static spark.Spark.*;
 
+
+import dao.Dao;
 import service.UsuarioService;
 
 public class Aplicacao {
 	
 	private static UsuarioService usuarioService = new UsuarioService();
-	
+	private static Dao dao = new Dao();
     public static void main(String[] args) {
         port(5432);
+		
+		
+		
+		dao.conectar();
 
+        
         post("/produto", (request, response) -> usuarioService.add(request, response));
 
         get("/produto/:id", (request, response) -> usuarioService.get(request, response));
@@ -20,6 +27,8 @@ public class Aplicacao {
         get("/produto/delete/:id", (request, response) -> usuarioService.remove(request, response));
 
         get("/produto", (request, response) -> usuarioService.getAll(request, response));
+        
+        dao.close();
                
     }
 }
